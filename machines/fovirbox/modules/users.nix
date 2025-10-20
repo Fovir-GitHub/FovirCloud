98b0c8f3dab55cd50c70292bc0971b0551678f82
Fovir <fovir@disroot.org>
2025-10-20 21:46:45 +0800

    chore: Restore to safe state.

---
+++ machines/fovirbox/modules/users.nix
@@ -0,0 +1,11 @@
+{...}: {
+  users.users.root = {
+    hashedPassword = "$y$j9T$oyJfMzGZz215hC/T.Xc/k/$eZdDxL9L0X/vUMfTQfFBrtOMtZfDnuksta8.4EG/1QB";
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOLstXknHcwZae+PSiXq1Wbz8Zmi4HF0UdWGNLMhuAJ fovir@disroot.org"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBFvfwOrz294r8TnRxnoHFHdg0ZghU5JoXExPouLIrHp fovir@disroot.org"
+      "md5sum id_ed25519 6020ff29ec245114b57436a97eeef3ea  id_ed25519"
+    ];
+  };
+}
